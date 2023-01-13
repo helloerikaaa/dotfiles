f3f7aa49b7da1bf6596c23a69caac7865a0c7ed6
Erika Sánchez-Femat <erikasafe@gmail.com>
2023-01-13 13:32:14 -0600

    [UPDATE] main script to delete raspberry pi [Author] Erika Sánchez Femat
    -- Deleted raspberry from option because of at the moment is not needed.

--- install.sh
+++ install.sh
@@ -3,22 +3,17 @@
 gum style \
 	--foreground 212 --border-foreground 212 --border double \
 	--align center --width 50 --margin "1 2" --padding "2 4" \
-	'Welcome to the Juno dotfiles utility.' 'Setup your MacOS and Raspberry Pi devices' 'Feedback: @helloerikaaa on github'
-
-echo "Select your OS"
-OS=$(gum choose "MacOS" "Raspbian OS" "Ubuntu OS" "Pi-Hole" "Astroberry")
+	'Welcome to the dotfiles utility.' 'Setup your MacOS and Raspberry Pi devices' 'Feedback: @helloerikaaa on github'
 
 echo "Select the dependencies to install"
 DEP=$(gum choose "All" "Neovim" "ZSH")
 
-echo "Installing $DEP dependencies on $OS"
-
-if [[ $OS == "MacOS" && $DEP == "All" ]]; then
+if [[ $DEP == "All" ]]; then
 	sh ./os/macos/macos.sh
 	sh ./os/macos/zsh.sh
 	sh ./os/macos/neovim.sh
-elif [[ $OS == "MacOS" && $DEP == "Neovim" ]]; then
+elif [[ $DEP == "Neovim" ]]; then
 	sh ./os/macos/neovim.sh
-elif [[ $OS == "MacOS" && $DEP == "ZSH" ]]; then
+elif [[ $DEP == "ZSH" ]]; then
 	sh ./os/macos/zsh.sh
 fi
